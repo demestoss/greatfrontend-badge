166fef7582dfceca18f080e3ab6303b9894a8f39
Demes Toss <me@demestoss.com>
2025-02-10 21:45:32 +0100

    Added Remix github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { copyFileSync } from 'node:fs';
+import { join } from 'node:path';
 import { vitePlugin as remix } from '@remix-run/dev';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
@@ -7,6 +9,24 @@ export default defineConfig({
     plugins: [
         remix({
             basename: '/greatfrontend-design-system/',
+            ssr: false,
+            buildEnd(args) {
+                if (!args.viteConfig.isProduction) return;
+
+                // When deploying to GitHub Pages, if you navigate from / to another
+                // route and refresh the tab, it will show the default GH Pages 404
+                // page. This happens because GH Pages is not configured to send all
+                // traffic to the index.html where we can load our client-side JS.
+                // To fix this, we can create a 404.html file that contains the same
+                // content as index.html. This way, when the user refreshes the page,
+                // GH Pages will serve our 404.html and everything will work as
+                //expected.
+                const buildPath = args.viteConfig.build.outDir;
+                copyFileSync(
+                    join(buildPath, 'index.html'),
+                    join(buildPath, '404.html'),
+                );
+            },
         }),
         tailwindcss(),
     ],
