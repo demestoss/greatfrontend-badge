e4470ffd8d5ee1e07d0455d0f599c1721a39087f
Demes Toss <me@demestoss.com>
2025-02-10 22:10:08 +0100

    Added index page

---
+++ app/routes/_index.tsx
@@ -0,0 +1,15 @@
+import { Link } from '@remix-run/react';
+import { Button } from '../components/Button';
+
+export default function Home() {
+    return (
+        <div className="w-60 flex flex-wrap items-center gap-6 py-[200px]">
+            <Link to="/badge">
+                <Button intent={'linkColor'}>Badge</Button>
+            </Link>
+            <Link to="/button">
+                <Button intent={'linkColor'}>Button</Button>
+            </Link>
+        </div>
+    );
+}
